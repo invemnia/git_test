a76ce814087f7d8a4f60d366a9118463a0a799d3
invemnia <invemnia@gmail.com>
2026-07-24 17:01:43 +1200

    lab 01

---
+++ learning_python/compsci130/Lab01_worksheet.py
@@ -0,0 +1,18 @@
+# ##---------------------------------------TASK-3---------------------------------------------
+
+country = {"father" : "Son", "Mother" : "Daughter"}
+length = input("length: ")
+
+for key, value in country.items():
+    print(f"{key:<{length}}{value:>{length}}")
+
+##---------------------------------------TASK-4.2---------------------------------------------
+# def get_squares(numbers):
+#     squares = []
+#     for numbes in numbers:
+#         squares.append(numbers**2)
+#     return squares
+
+
+##---------------------------------------TASK-5.1---------------------------------------------
+def
