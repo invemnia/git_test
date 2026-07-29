349d98362e7acb999e1a11607cba983b010ca68f
invemnia <invemnia@gmail.com>
2026-07-29 15:50:09 +1200

    bluh

--- python_learning/compsci130/lab01/lab01_stuff.py
+++ python_learning/compsci130/lab01/lab01_stuff.py
@@ -11,5 +11,40 @@
 
 
 
+# #------------------------------PRE-LAB---------------------------------
+
+###### Question 6
 
 
+###### Question 10
+##--attempt 1
+# numbers = [12, 122, 3, 32, 14, 2, 400, 1, 42]
+# new_numbers = []
+
+# for i in numbers:
+#     if i < 10:
+#         numbers.remove(i)
+# print(numbers)
+
+#--atempt2: works but copies list
+# numbers = [12, 122, 3, 32, 14, 2, 400, 1, 42]
+# new_numbers = []
+
+# def remove_10(numbers):
+#     for i in numbers:
+#         if i < 10:
+#             new_numbers.append(i)
+#     print(new_numbers)
+
+
+#attempt3:
+numbers = [12, 122, 3, 32, 14, 2, 400, 1, 42]
+
+def remove_10(numbers):
+    for i in numbers.copy():
+        if i > 10:
+            numbers.remove(i)
+    return numbers
+print(numbers)
+print(remove_10(numbers))
+    
--- python_learning/compsci130/lab02/lab02.py
+++ python_learning/compsci130/lab02/lab02.py
@@ -1,18 +1,56 @@
 #----------------------------Q-8-------------------------------#
-# while user
-# prompt("Enter an integer: ")
+# ---attempt1: 
+
+# def get_valid_integer(prompt, min_value, max_value):
+#     while True:
+#         value = int(input(prompt))
+#         if (min_value < value < max_value) or (max_value > value > min_value):
+#             return value
+#             break   
+            
+
+    
+# number = get_valid_integer("Enter an integer: ", 1, 5)
+# print(number)
 
 
-#----------------------------Q-12-------------------------------#
-a_dict = {'Kaitaia': [233.8, 199.6, 200.0, 163.9, 146.5, 127.4, 139.5, 161.3, 169.7, 194.7, 193.7, 202.2], 
-'Auckland': [240.3, 203.4, 200.8, 169.3, 149.1, 126.1, 133.9, 153.7, 159.0, 180.5, 203.8, 201.9], 
-'Tauranga': [269.0, 221.5, 221.4, 184.3, 170.9, 134.1, 149.9, 176.4, 178.2, 214.8, 240.0, 241.0]}
+# # --Atempt 2: (works). Removed break and made it inclusive
+# def get_valid_integer(prompt, min_value, max_value):
+    
+#     while True:
+#         value = int(input(prompt))
+#         if (min_value <= value <= max_value) or (max_value >= value >= min_value):
+#             return value
+            
 
-def print_data(sunshine_dict):
-    for locations in sunshine_dict.keys():
-        print(locations)
-        for values in sunshine_dict.values():
-            print(values)
+    
+# number = get_valid_integer("Enter an integer: ", 1, 5)
+# print(number)
 
-print_data(a_dict)
+
+#making it better 
+def get_valid_integer(prompt, min_value, max_value):
+    
+    while True:
+        value = int(input(prompt))
+        if min_value <= value <= max_value:
+            return value
+            
+
+    
+number = get_valid_integer("Enter an integer: ", 1, 5)
+print(number)
+
+#     #----------------------------Q-12-------------------------------#
+# a_dict = {'Kaitaia': [233.8, 199.6, 200.0, 163.9, 146.5, 127.4, 139.5, 161.3, 169.7, 194.7, 193.7, 202.2], 
+# 'Auckland': [240.3, 203.4, 200.8, 169.3, 149.1, 126.1, 133.9, 153.7, 159.0, 180.5, 203.8, 201.9], 
+# 'Tauranga': [269.0, 221.5, 221.4, 184.3, 170.9, 134.1, 149.9, 176.4, 178.2, 214.8, 240.0, 241.0]}
+
+# def print_data(sunshine_dict):
+#     for locations in sunshine_dict.keys():
+#         print(locations)
+#         for values in sunshine_dict.values():
+#             print(values)
+
+# print_data(a_dict)
 
